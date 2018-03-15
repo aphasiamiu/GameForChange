6e8ccdf661d51d73619ba6235275de39cdbbfc53
ANDREW\shanj <cmujiangshan@gmail.com>
2018-03-15 11:08:36 -0400

    1

--- Assets/Scripts/AIOutside.cs
+++ Assets/Scripts/AIOutside.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class AIOutside : MonoBehaviour
 {
-    public int size = 2;
+    public int size = 1;
     public Transform Cubepre;
     // Use this for initialization
     void Start()
     {
-        float angle = 0.0f;
+        float angle = 0.1f;
         float p = 3.141592653589793f;
         float step = 1.0f;
-        
+
         for (int x = 0; x < 20; x++)
         {
             angle = angle + p / (10.0f);
 
             Instantiate(Cubepre, new Vector3(Mathf.Sin(angle) * 4 * size, 0, Mathf.Cos(angle) * 4 * size), Quaternion.identity);
             Instantiate(Cubepre, new Vector3(Mathf.Sin(angle) * 3 * size, 0, Mathf.Cos(angle) * 3 * size), Quaternion.identity);
-            Instantiate(Cubepre, new Vector3(Mathf.Sin(angle) * 5 * size, 0, Mathf.Cos(angle) * 2 * size), Quaternion.identity);
+
         }
         angle = p / 15.0f;
         for (int x = 0; x < 20; x++)
@@ -29,6 +29,8 @@ public class AIOutside : MonoBehaviour
             Instantiate(Cubepre, new Vector3(Mathf.Sin(angle) * 9 * size / 2, 0, Mathf.Cos(angle) * 9 * size / 2), Quaternion.identity);
 
             Instantiate(Cubepre, new Vector3(Mathf.Sin(angle) * 7 * size / 2, 0, Mathf.Cos(angle) * 7 * size / 2), Quaternion.identity);
+
+            Instantiate(Cubepre, new Vector3(Mathf.Sin(angle) * 5 * size / 2, 0, Mathf.Cos(angle) * 5 * size / 2), Quaternion.identity);
         }
     }
 
