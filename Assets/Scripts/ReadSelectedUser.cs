d92c3efc4cbcc343b4a78e0ea921d057a33ba3f8
ANDREW\shanj <cmujiangshan@gmail.com>
2018-04-17 13:04:32 -0400

    new

--- Assets/Scripts/ReadSelectedUser.cs
+++ Assets/Scripts/ReadSelectedUser.cs
@@ -11,10 +11,13 @@ public class ReadSelectedUser : MonoBehaviour
     public static ReadSelectedUser Instance;
     public float time;
     public int i = 0;
+    public int j = 0;
     public List<string> names = new List<string>();
     public List<int> points = new List<int>();
     public List<string> sortnames = new List<string>();
     public List<int> sortpoints = new List<int>();
+    public List<int> hashpoints = new List<int>();
+    public List<string> hashtags = new List<string>();
     public GameObject text;
     private int startNum;
     private int count;
@@ -51,6 +54,8 @@ public class ReadSelectedUser : MonoBehaviour
          
             string name = N["names"][i];
             int point = N["points"][i];
+            string hashtag = N["hashtags"][j];
+            int hashpoint = N["hashpoints"][j];
             
             if (point != 0) {
                 names.Add(name);
@@ -61,7 +66,17 @@ public class ReadSelectedUser : MonoBehaviour
                 //print(name);
                 //print(point);
             }
-           for(int j = 0; j < i; j = j + 1)
+            if (hashpoint != 0)
+            {
+                hashpoints.Add(hashpoint);
+                hashtags.Add(hashtag);
+                //sortnames.Add("");
+                //sortpoints.Add(0);
+                j = j + 1;
+                //print(name);
+                //print(point);
+            }
+            for (int j = 0; j < i; j = j + 1)
             {
                 names[j]= N["names"][j];
                 points[j]= N["points"][j];
---
+++ Assets/Scripts/hashtag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hashtag : MonoBehaviour {
+    public Text tag;
+    public float t;
+    // Use this for initialization
+    void Start () {
+        t = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        tag.GetComponent<Text>().text = ReadSelectedUser.Instance.hashtags[int.Parse(transform.parent.name) - 1];
+        if (Time.time - t > 1)
+        {
+            t = Time.time;
+            int i = Random.Range(-10, 10);
+            this.tag.fontSize += i;
+        }
+        
+	}
+}
