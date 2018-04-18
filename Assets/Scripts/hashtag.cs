a50af5847777608f6cef6d31bde294c6868eab7e
ANDREW\shanj <cmujiangshan@gmail.com>
2018-04-18 13:11:04 -0400

    new ui

--- Assets/Scripts/hashtag.cs
+++ Assets/Scripts/hashtag.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class hashtag : MonoBehaviour {
     public Text tag;
     public float t;
+    public int maxfont = 100;
+    public int minfont = 10;
     // Use this for initialization
     void Start () {
         t = Time.time;
@@ -17,9 +19,17 @@ public class hashtag : MonoBehaviour {
         if (Time.time - t > 1)
         {
             t = Time.time;
-            //int i = Random.Range(-10, 10);
-            this.tag.fontSize += (ReadSelectedUser.Instance.hashpoints[int.Parse(transform.parent.name) - 1]);
-            
+            int i = Random.Range(-10, 10);
+            //this.tag.fontSize += (ReadSelectedUser.Instance.hashpoints[int.Parse(transform.parent.name) - 1]);
+            this.tag.fontSize += i;
+            if (this.tag.fontSize > maxfont)
+            {
+                this.tag.fontSize = maxfont;
+            }
+            if (this.tag.fontSize < minfont)
+            {
+                this.tag.fontSize = minfont;
+            }
         }
         
 	}
