8989d19cf24828d307c1bd50074fd581bb0b7d1e
ANDREW\shanj <cmujiangshan@gmail.com>
2018-04-25 20:18:03 -0400

    new

---
+++ Assets/Scripts/top3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class top3 : MonoBehaviour {
+    
+    public GameObject name;
+    public float t;
+    public AudioSource cheer0;
+    public AudioSource cheer1;
+    public AudioSource cheer2;
+    
+    void Start()
+    {
+        name = gameObject;
+        t = Time.time;
+    }
+    // Update is called once per frame
+    void Update () {
+        if(Time.time - t > 1)
+        {
+            t = Time.time;
+            if(ReadSelectedUser.Instance.sortpoints.Count >= int.Parse(transform.parent.name))
+            {
+                if (name.transform.parent.Find("score").GetComponent<Text>().text != ReadSelectedUser.Instance.sortpoints[int.Parse(transform.parent.name) - 1].ToString())
+                {
+                    int r = Random.Range(0, 2);
+                    if (r == 0) { cheer0.Play(); }
+                    if (r == 1) { cheer1.Play(); }
+                    if (r == 2) { cheer2.Play(); }
+                    name.GetComponent<Text>().text = ReadSelectedUser.Instance.sortnames[int.Parse(transform.parent.name) - 1];
+                    name.transform.parent.Find("score").GetComponent<Text>().text = ReadSelectedUser.Instance.sortpoints[int.Parse(transform.parent.name) - 1].ToString();
+                }
+            }
+            
+            
+        }
+        
+    }
+}
